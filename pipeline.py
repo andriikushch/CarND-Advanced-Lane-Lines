a64c8d1e4134dbdeb80269d2fc249e9a8a76f497
Andrii Kushch <andrii.kushch@gmail.com>
2019-06-06 21:39:47 +0200

    add pipeline

--- pipeline.py
+++ pipeline.py
@@ -1,36 +1,62 @@
-def get_color_in_hls(rgb_color):
-    r = rgb_color[0]
-    g = rgb_color[1]
-    b = rgb_color[2]
+import numpy as np
+import cv2
+from calibration import *
+from helpers import *
 
-    v_max = max(rgb_color)
-    v_min = min(rgb_color)
 
-    if v_max == r:
-        h = 30 * (g - b) / (v_max - v_min)
-    elif v_max == g:
-        h = 60 + 30 * (b - r) / (v_max - v_min)
-    else:
-        h = 120 + 30 * (r - g) / (v_max - v_min)
+def pipeline(image):
+    # copy original image     
+    img = np.copy(image)
 
-    l = (v_max + v_min) / 2
+    # undistort image     
+    undistorted_image = cv2.undistort(image, mtx, dist, None, mtx)
 
-    if l < 0.5:
-        s = (v_max - v_min) / (v_max + v_min)
-    else:
-        s = (v_max - v_min) / (2 - v_max + v_min)
+    # create "bird view"     
+    transformed_image = transform_image(undistorted_image, M1)
+    # filter yellow and white colors     
+    filtered_image = filter_line_colors(transformed_image)
+    # create gray image
+    gray = cv2.cvtColor(filtered_image, cv2.COLOR_BGR2GRAY)
 
-    return [h, l, s]
+    # create binary image
+    binary = np.zeros_like(gray)
+    binary[gray > 0] = 1
 
-#
-# def hls_select(img, channel=0, thresh=(0, 255)):
-#     hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
-#     ch = hls[:,:,channel]
-#     binary_output = np.zeros_like(ch)
-#     binary_output[(ch > thresh[0]) & (ch <= thresh[1])] = 1
-#     return binary_output#
-#
-# def pipeline(input_image):
-#     print("hi")
+    try:
+        # fit poly     
+        left_line, right_line, out_image, left_poly, right_poly = fit_polynomial(binary)
+    except TypeError as e:
+        return image, image, image
 
-print(get_color_in_hls([230,254,244]))
+    # for debug purpose     
+    original_out = np.copy(out_image)
+
+    # calcualte car position
+    f_l = np.poly1d(left_poly)
+    f_r = np.poly1d(right_poly)
+    middle = img.shape[1] / 2
+    text = "CAR DISTANCE TO LEFT LINE : {0} CAR DISTANCE TO RIGHT LINE : {1}".format(middle - f_l(img.shape[1]),
+                                                                                     f_r(img.shape[1]) - middle)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(img, text, (10, 30), font, 0.7, (255, 255, 255), 2, cv2.LINE_AA)
+
+    # draw lines
+    ploty = np.linspace(0, img.shape[0] - 1, img.shape[0])
+
+    # collect points for lines and poly  
+    left_line_points = np.dstack((left_line, ploty))[0]
+    right_line_points = np.dstack((right_line, ploty))[0]
+
+    poly_points = np.concatenate((np.int32(left_line_points)[::-1], np.int32(right_line_points)))
+
+    cv2.polylines(out_image, np.int32([left_line_points]), False, (255, 150, 0), 140)
+    cv2.polylines(out_image, np.int32([right_line_points]), False, (0, 150, 255), 140)
+    cv2.fillPoly(out_image, np.int32([poly_points]), (0, 255, 0))
+
+    # convert "bird view" to normal
+    normal_image = transform_image(out_image, M2)
+
+    # Combine the result with the original image
+    result = cv2.addWeighted(img, 1, normal_image, 0.6, 0)
+
+    return result, transformed_image, original_out
